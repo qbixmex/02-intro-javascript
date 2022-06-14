9001b2d5c27f756f88243f71ad3dfc1e522e9c18
Daniel González Briseño <daniel.gb@globant.com>
2022-06-14 12:58:48 -0500

    02 - Let and Const Variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import './index.css';
 
-console.log('React JS');
+/* =================================== */
+/* ===== VARIABLES AND CONSTANTS ===== */
+/* =================================== */
+
+// A constant is a variable type that
+// won't change because it is inmutable.
+const year = 2022;
+
+console.log('Year:', year);
+
+// A let is a variable type that
+// you can change because it is mutable.
+let quantity; // Initial assign by default is undefined
+
+console.log('Quantity:', quantity);
+
+quantity = 25; // You can change value
+
+console.log('Quantity:', quantity);
+
+console.log('='.repeat(50));
+
+// const and let variables are scoped to curly braces.
+if (true) {
+
+  const year = 2022;
+  console.log('Year:', year);
+
+  let quantity = 12;
+  console.log('Quantity:', quantity);
+
+}
+
+console.log('='.repeat(50));
+
+// Note, this variables keeps its value
+// because variables inside curly braces
+// has different scope.
+console.log('Quantity:', quantity);
