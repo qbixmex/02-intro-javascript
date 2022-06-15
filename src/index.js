a8a1dc351ac003ec1ea33d0b430fa21e10a10524
Daniel González Briseño <daniel.gb@globant.com>
2022-06-15 15:14:56 -0500

    07 - Object Destructuring

--- src/index.js
+++ src/index.js
@@ -1,42 +1,98 @@
 import './index.css';
 
-/* ===================================== */
-/* ============= Functions ============= */
-/* ===================================== */
+/* ========================================= */
+/* ============= Destructuring ============= */
+/* ========================================= */
 
-// Method 1
-function greeting1( name ) {
-  return `Hello ${name}`;
-}
-
-// Method 2
-const greeting2 = function( name ) {
-  return `Hello ${name}`;
+const superhero = {
+  name: 'Bruce Wayne',
+  age: 45,
+  hero: 'Batman'
 };
 
-// Method 3
-const greeting3 = ( name ) => {
-  return `Hello ${name}`;
+const { name, age, hero } = superhero;
+
+console.log( 'Name:', name );
+console.log( 'Age:', age );
+console.log( 'Hero:', hero );
+
+console.log( '='.repeat(40) );
+
+const student = {
+  name: 'Alex Stewart',
+  age: 22,
+  course: 'Javascript',
 };
 
-// Method 4
-const greeting4 = name => `Hello ${name}`;
+const getStudent = ({ name, age, course, active = false }) => {
+  return { name, age, course, active };
+};
 
-// Method 5
-const generalGreeting = () => 'Hello World';
+const studentRetrieved = getStudent(student);
 
-// Method 6
-const getUser = (id) => ({
-  uid: id,
-  username: 'elixir',
-  email: 'elixir@gmail.com'
+console.table( studentRetrieved );
+
+console.log( '='.repeat(40) );
+
+const context = ({
+  name,
+  description,
+  price,
+  active = false,
+  sizes: {
+    width,
+    height,
+    weight
+  }
+}) => {
+  return {
+    'product-name': name,
+    'product-description': description,
+    'product-price': price,
+    'product-status': active,
+    'product-sizes': {
+      'product-width': width,
+      'product-height': height,
+      'product-weight': weight,
+    },
+  };
+};
+
+const keys = [
+  'product-name',
+  'product-description',
+  'product-price',
+  'product-status',
+];
+
+const {
+  [keys[0]]: productName,
+  [keys[1]]: productDescription,
+  [keys[2]]: productPrice,
+  [keys[3]]: productStatus,
+  'product-sizes': productSizes
+} = context({
+  name: 'Play Station 5',
+  description: 'Lorem ipsum dolor dolem',
+  price: 1135.25,
+  sizes: {
+    width: 4,
+    height: 8,
+    weight: 22
+  }
 });
 
-const user = getUser('ABC123');
+const {
+  'product-width': productWidth,
+  'product-height': productHeight,
+  'product-weight': productWeight,
+} = productSizes;
 
-console.log(greeting1('Goku'));
-console.log(greeting2('Vejita'));
-console.log(greeting3('Piccolo'));
-console.log(greeting4('Bulma'));
-console.log(generalGreeting());
-console.table(user);
+console.log('Product Name:', productName);
+console.log('Product Description:', productDescription);
+console.log('Product Price: $', productPrice.toLocaleString());
+console.log('Product Status:', productStatus);
+console.log('-'.repeat(10), 'Product Sizes', '-'.repeat(10));
+console.log('Product Width:', productWidth);
+console.log('Product Height:', productHeight);
+console.log('Product Weight:', productWeight);
