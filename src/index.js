f709b3999ba2b5a95a5e37b350d46d9b57137411
Daniel González Briseño <daniel.gb@globant.com>
2022-06-16 00:11:19 -0500

    14 - Fetch API (Async, Await)

--- src/index.js
+++ src/index.js
@@ -4,15 +4,43 @@ import './index.css';
 /* ============= FETCH API ============= */
 /* ===================================== */
 
-const apiKey = 'oomM6BITz3sj9qYBePGd2lsEYVvEaQzc';
-const request = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+const getImagePromise = (exists = true) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      exists 
+        ? resolve('https://somedomain.com')
+        : reject('Domain does not exist!')
+    }, 1500);
+  });
+};
 
-request
-  .then(response => response.json())
-  .then(({ data }) => {
-    const { url } = data.images.original;
-    const img = document.createElement('img');
-    img.src = url;
-    document.body.append(img);
+getImagePromise()
+  .then((url) => {
+    console.log('-'.repeat(12), 'THEN - CATCH', '-'.repeat(12));
+    console.log('URL:', url)
   })
   .catch(console.error);
+
+  
+const getGiphy = async () => {
+  try {
+
+    const apiKey = 'oomM6BITz3sj9qYBePGd2lsEYVvEaQzc';
+    const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+    const { data } = await response.json();
+  
+    const { url } = data.images.original;
+    
+    return url;
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async() => {
+  const url = await getGiphy();
+  const img = document.createElement('img');
+  img.src = url;
+  document.body.append(img);
+})();
